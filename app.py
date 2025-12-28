a0d913127f413a5b27994b925be2ac1565583fbb
AybukeKuruyuz <kuruyuzaybuke6@gmail.com>
2025-12-29 00:43:53 +0300

    Network oyunu oyun sayfasına yerleştirildi

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "EduPlay Hub Projesi Calisiyor! Merhaba Takim."
+    return render_template("index.html")
+
+@app.route("/ortaokul")
+def ortaokul():
+    return render_template("ortaokul.html")
+
+@app.route("/ilkokul")
+def ilkokul():
+    return render_template("ilkokul.html")
+
+@app.route("/play/<game_id>")
+def play(game_id):
+    return render_template("game.html", game_id=game_id)
+
+@app.route("/embed/<game_id>")
+def embed(game_id):
+    return render_template(f"games/{game_id}.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
